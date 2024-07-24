d6525cc9ad883f397c7a428c19e0ccbd2d029c8e
Annie Rhodes <annierhodes17@gmail.com>
2024-07-24 11:15:18 +0000

    added print menu and add expenses python

--- run.py
+++ run.py
@@ -12,8 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('personal-budget-manager')
 
-income = SHEET.worksheet('income')
+INCOME_SHEET = SHEET.worksheet('income')
+EXPENSES_SHEET = SHEET.worksheet('expenses')
+SUMMARY_SHEET = SHEET.worksheet('summary')
 
-data = income.get_all_values()
 
-print(data)
+def addExpense(amount, catagory):
+    expense = {'amount': amount, {catagory}: catagory}
+    expense.append(expense)
+
+
+def printMenu():
+    print('Please choose from one of the following options... ')
+    print('Add Monthly Income')
+    print('Add Expense')
+    print('Remove Expense')
+    print('List expenses')
+    print('View Summary')
